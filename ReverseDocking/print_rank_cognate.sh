1b80151384401f582695e463303c7c70a0460ec9
Christopher Corbo <chris.corbo.xx@gmail.com>
2024-03-02 18:04:28 -0500

    Updating reverse docking scripts

--- ReverseDocking/print_rank_cognate.sh
+++ ReverseDocking/print_rank_cognate.sh
@@ -3,16 +3,16 @@
 #SBATCH --time=12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=20
-#SBATCH --job-name=Whaddup
-#SBATCH --output=boop
+#SBATCH --job-name=print_rank_cog
+#SBATCH --output=print_rank_cog.out
 for lig_sys in `cat FARMA.systems.all`;do
 cd $lig_sys
-rm tmp_scores.txt
+rm tmp_scores_cog.txt
 for sys in `cat ../FARMA.systems.all `;do
 val=`grep Grid_Score ${lig_sys}_${sys}_0_scored.mol2| head -n1`
-echo $sys " " $val >> tmp_scores.txt
+echo $sys " " $val >> tmp_scores_cog.txt
 done
-rank=`sort -n -k4  tmp_scores.txt| grep -n $lig_sys| awk -F ':' '{print $1}'`
+rank=`sort -n -k4  tmp_scores_cog.txt| grep -n $lig_sys| awk -F ':' '{print $1}'`
 echo $lig_sys " " $rank >> ../zzy.cognate_rec_rank.dat
 cd ..
 done
--- ReverseDocking/print_rank_protein_pairs.sh
+++ ReverseDocking/print_rank_protein_pairs.sh
@@ -3,20 +3,20 @@
 #SBATCH --time=12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=28
-#SBATCH --job-name=Whaddup
-#SBATCH --output=boop
+#SBATCH --job-name=print_rank_pro
+#SBATCH --output=print_rank_pro.out
 for lig_sys in `cat FARMA.systems.all`;do
 cd $lig_sys
-rm tmp_scores.txt
+rm tmp_scores_propairs.txt
 for sys in `cat ../FARMA.systems.all `;do
 val=`grep Grid_Score ${lig_sys}_${sys}_0_scored.mol2| head -n1`
-echo $sys " " $val >> tmp_scores.txt
+echo $sys " " $val >> tmp_scores_propairs.txt
 done
 
 pairs=`grep $lig_sys ../../FARMA_pairs.dat `
-best_pair=`for pair in echo $pairs;do grep $pair tmp_scores.txt ;done | sort -n -k4 | head -n1 | awk '{print $1}'`
+best_pair=`for pair in echo $pairs;do grep $pair tmp_scores_propairs.txt ;done | sort -n -k4 | head -n1 | awk '{print $1}'`
 
-rank=`sort -n -k4  tmp_scores.txt| grep -n $best_pair | awk -F ':' '{print $1}'`
+rank=`sort -n -k4  tmp_scores_propairs.txt| grep -n $best_pair | awk -F ':' '{print $1}'`
 echo $lig_sys " " $rank >> ../zzy.cognate_rec_rank_w_pairs.dat
 cd ..
 done
